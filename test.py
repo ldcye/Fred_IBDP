f78251cae58e62e77e522babd30294eb05ae8a44
Fred <huipengchen605@gmail.com>
2025-09-08 08:43:56 +0800

    test day 9.8

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
 print("Hello world")
 for i in range(1,5):
     print("Goodbye world")
+#write anything sensable here for a test file
+print("Hello again")
+for i in range(1,5):
+    print("Goodbye again")
+print("The end")
