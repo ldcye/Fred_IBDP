6a7f261878e99df6543148286f662395feba9056
Fred <huipengchen605@gmail.com>
2025-09-08 21:33:22 +0800

    HW_DATE_9.8

--- test.py
+++
@@ -1,8 +0,0 @@
-print("Hello world")
-for i in range(1,5):
-    print("Goodbye world")
-#write anything sensable here for a test file
-print("Hello again")
-for i in range(1,5):
-    print("Goodbye again")
-print("The end")
---
+++ try1.py
@@ -0,0 +1,22 @@
+# math = 40%
+# science = 35%
+# english = 25%
+
+math = input("Enter math score: ")
+science = input("Enter science score: ")
+english = input("Enter english score: ")
+
+final_score = (int(math) * 0.4) + (int(science) * 0.35) + (int(english) * 0.25)
+
+if final_score >= 60:
+    sucess = "pass"
+else:
+    sucess = "fail"
+
+print("Student\'s total weighted score is: " + str(final_score)+ sucess"\n")
+
+print("Student\'s"+ "total"+ "weighted"+ "score"+ "is:" + str(final_score)+ sucess"\n")
+
+print(" %s %s %s %s %s %.1f" % ("Student's total weighted score is: ", final_score, " ", sucess, "\n"))
+
+print(f"Student's total weighted score is: ", ({final_score}), {sucess}"\n")
